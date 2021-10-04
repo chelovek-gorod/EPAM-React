a74b0b202a40b2d9c7d0556ccabf288d51ea9067
Marsel <chelovek-gorod@yandex.by>
2021-10-04 09:41:02 +0300

    Create new components

--- src/App/App.js
+++ src/App/App.js
@@ -1,35 +1,14 @@
-import user from '../Userinfo/User';
-import avatar from '../img/avatar.png';
+import UserDetails from '../UserDetails/UserDetails';
+import Content from '../Content/Content';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      <div className="UserDetails border">
+      <UserDetails />
 
-        <div className="Avatar border">
-          <img src={avatar} alt="Avatar"/>
-        </div>
-
-        <div className="Userinfo border">
-          <div className="user-line">name : <span className="line-data">{user.name}</span></div>
-          <div className="user-line">user name : <span className="line-data">{user.username}</span></div>
-          <div className="user-line">email : <span className="line-data">{user.email}</span></div>
-          <div className="user-line">address : <span className="line-data">
-            {user.address.street}, {user.address.suite}<br/>{user.address.city}, {user.address.zipcode}</span></div>
-          <div className="user-line">phone : <span className="line-data">{user.phone}</span></div>
-          <div className="user-line">company name : <span className="line-data">{user.company.name}</span></div>
-        </div>
-      
-      </div>
-
-      <div className="Content border">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
+      <Content />
       
     </div>
   );
